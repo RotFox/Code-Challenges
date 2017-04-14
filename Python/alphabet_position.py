89e8ba7055eb898b27ed99ee371e186210bf53e9
rotfox <rotfox@github.com>
2017-04-14 16:49:11 -0700

    added alphabet_position and test

--- Python/alphabet_position.py
+++ Python/alphabet_position.py
@@ -1,2 +1,4 @@
 def alphabet_position(text):
+    ''' alphabet_position returns a space separated string containing the 
+    numerical position of alpha numeric characters in a given string. '''
     return " ".join([str(ord(c)&31) for c in text if c.isalpha()])
